45deb45f34598a77fdd3dc7585101f813e7b6f5d
akashannex <akash@annexlogics.com>
2020-12-30 23:13:26 +0530

    Updates

---
+++ precache-manifest.82456befabe1c7b847b9584722461e24.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9b319f5628429d0d2d4a185dd4aa5d",
+    "url": "/schoollivewebsite/index.html"
+  },
+  {
+    "revision": "9fbd0969d5da40f49c3d",
+    "url": "/schoollivewebsite/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "b38a7fb6682600ae703d",
+    "url": "/schoollivewebsite/static/css/main.62f90cbd.chunk.css"
+  },
+  {
+    "revision": "9fbd0969d5da40f49c3d",
+    "url": "/schoollivewebsite/static/js/2.5f87485d.chunk.js"
+  },
+  {
+    "revision": "b38a7fb6682600ae703d",
+    "url": "/schoollivewebsite/static/js/main.1197ef02.chunk.js"
+  },
+  {
+    "revision": "bce00d2e077fe301410d",
+    "url": "/schoollivewebsite/static/js/runtime~main.510f3ffb.js"
+  },
+  {
+    "revision": "71668fcf75fb736bfc62595711fa53b8",
+    "url": "/schoollivewebsite/static/media/02.71668fcf.png"
+  },
+  {
+    "revision": "4279b8e5dc5d2d7065aaf27e543487ac",
+    "url": "/schoollivewebsite/static/media/11.4279b8e5.png"
+  },
+  {
+    "revision": "8156f3d505d33b16e3dd4f81519e2038",
+    "url": "/schoollivewebsite/static/media/111.8156f3d5.png"
+  },
+  {
+    "revision": "c9c57e0ce9729bc8e8f829ad06dfcce7",
+    "url": "/schoollivewebsite/static/media/1parent.c9c57e0c.jpg"
+  },
+  {
+    "revision": "aa837f628e2f40ee1ec2eb03e9a725b1",
+    "url": "/schoollivewebsite/static/media/2parent.aa837f62.jpg"
+  },
+  {
+    "revision": "c6da6720cc9b23da0dd024858818a1ef",
+    "url": "/schoollivewebsite/static/media/3parent.c6da6720.jpg"
+  },
+  {
+    "revision": "7f1714ecb074a77d51d4063b0f79dc9c",
+    "url": "/schoollivewebsite/static/media/Activity-fun.7f1714ec.png"
+  },
+  {
+    "revision": "072818895fea0f2656defb7a3bf8f421",
+    "url": "/schoollivewebsite/static/media/Arjun School Visiting Back.07281889.jpg"
+  },
+  {
+    "revision": "07d0b1ee1a81cdfd7136bcd520f34480",
+    "url": "/schoollivewebsite/static/media/Art and craft.07d0b1ee.png"
+  },
+  {
+    "revision": "7d1b95799991629f1dafa421590ff67a",
+    "url": "/schoollivewebsite/static/media/Kindergarten-school.7d1b9579.png"
+  },
+  {
+    "revision": "84dde9d1fcaafa6bb2e5c92a9e3dd539",
+    "url": "/schoollivewebsite/static/media/Senior School.84dde9d1.png"
+  },
+  {
+    "revision": "a004b4b6348809a51f1222d3545a3557",
+    "url": "/schoollivewebsite/static/media/Time.a004b4b6.png"
+  },
+  {
+    "revision": "ed9ce3593972fcc08041fdf215557518",
+    "url": "/schoollivewebsite/static/media/aboutus.ed9ce359.JPG"
+  },
+  {
+    "revision": "b701a0bbf6ae33d3d8ae142ad7a52bd4",
+    "url": "/schoollivewebsite/static/media/activity.b701a0bb.png"
+  },
+  {
+    "revision": "1e8f581e49c62eddeadef8365a022842",
+    "url": "/schoollivewebsite/static/media/admission.1e8f581e.jpg"
+  },
+  {
+    "revision": "e05782097ef0ac11747626d28afb440f",
+    "url": "/schoollivewebsite/static/media/bg2.e0578209.png"
+  },
+  {
+    "revision": "ff1b0dde22762f61d8d44df9fcb459ae",
+    "url": "/schoollivewebsite/static/media/breadcrumb.ff1b0dde.jpg"
+  },
+  {
+    "revision": "dac4d48081e4ae907d60a41775e8e1a1",
+    "url": "/schoollivewebsite/static/media/building.dac4d480.JPG"
+  },
+  {
+    "revision": "92cff80e523dc5de35602fe8f46df214",
+    "url": "/schoollivewebsite/static/media/classroom.92cff80e.png"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/schoollivewebsite/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "85cb643a82db4f9d73ea2789859a18d7",
+    "url": "/schoollivewebsite/static/media/contactbg.85cb643a.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/schoollivewebsite/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/schoollivewebsite/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "0d9b90458b75df163b9f3033388183ee",
+    "url": "/schoollivewebsite/static/media/features.0d9b9045.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/schoollivewebsite/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "f3abca668fc8f1e7ce6659bdf4b15220",
+    "url": "/schoollivewebsite/static/media/fun.f3abca66.jpg"
+  },
+  {
+    "revision": "05b4af6bf9753a385ed9ab324f227bcb",
+    "url": "/schoollivewebsite/static/media/hostel.05b4af6b.jpg"
+  },
+  {
+    "revision": "ded9c787448442d5182ff46a5dbf797f",
+    "url": "/schoollivewebsite/static/media/hostel1.ded9c787.jpg"
+  },
+  {
+    "revision": "53093e6c5da5b29ccea6a11589a68c43",
+    "url": "/schoollivewebsite/static/media/hostel10.53093e6c.jpg"
+  },
+  {
+    "revision": "3499144d3c0933709a0301131bfe81be",
+    "url": "/schoollivewebsite/static/media/hostel2.3499144d.jpg"
+  },
+  {
+    "revision": "7cfcdbe5441a9c81092c6668015d1176",
+    "url": "/schoollivewebsite/static/media/hostel3.7cfcdbe5.jpg"
+  },
+  {
+    "revision": "40f5caf6268d997d58db8d68118e5d8a",
+    "url": "/schoollivewebsite/static/media/hostel4.40f5caf6.jpg"
+  },
+  {
+    "revision": "53de712cbebca1f432a5367b82a10b02",
+    "url": "/schoollivewebsite/static/media/hostel5.53de712c.jpg"
+  },
+  {
+    "revision": "d1054b457d1be882fa2b509dd653f9e8",
+    "url": "/schoollivewebsite/static/media/hostel6.d1054b45.jpg"
+  },
+  {
+    "revision": "4b4fe47b19fc6ad7e7f0b880852158a3",
+    "url": "/schoollivewebsite/static/media/hostel7.4b4fe47b.jpg"
+  },
+  {
+    "revision": "40f5caf6268d997d58db8d68118e5d8a",
+    "url": "/schoollivewebsite/static/media/hostel8.40f5caf6.jpg"
+  },
+  {
+    "revision": "ad8ca2f5f55f4f84f6ee5356f1d1f811",
+    "url": "/schoollivewebsite/static/media/hostel9.ad8ca2f5.jpg"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/schoollivewebsite/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "6756564b4869f92f20abf134f0c56d26",
+    "url": "/schoollivewebsite/static/media/img-2.6756564b.jpg"
+  },
+  {
+    "revision": "4ccc124f813dc4422a8070481c758348",
+    "url": "/schoollivewebsite/static/media/intellectual.4ccc124f.png"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/schoollivewebsite/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/schoollivewebsite/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/schoollivewebsite/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/schoollivewebsite/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "ce9d5a93705df680ac19862be83033e2",
+    "url": "/schoollivewebsite/static/media/library1.ce9d5a93.jpg"
+  },
+  {
+    "revision": "9ef80ae7aaaa290c03f4d4cab2307530",
+    "url": "/schoollivewebsite/static/media/library2.9ef80ae7.jpg"
+  },
+  {
+    "revision": "f8d84c992f95df6a32d214098c333e1e",
+    "url": "/schoollivewebsite/static/media/library3.f8d84c99.jpg"
+  },
+  {
+    "revision": "6256fc83323d6455165a5c2dafafaf19",
+    "url": "/schoollivewebsite/static/media/library4.6256fc83.jpg"
+  },
+  {
+    "revision": "0331529b2e9ea2f71f8d7be2fcfce8f7",
+    "url": "/schoollivewebsite/static/media/library5.0331529b.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/schoollivewebsite/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "8d57180197e387e8f6091a971e202de7",
+    "url": "/schoollivewebsite/static/media/logo.8d571801.JPG"
+  },
+  {
+    "revision": "cfc806a7eed96c7aebeddfa46438e8fb",
+    "url": "/schoollivewebsite/static/media/middle-school.cfc806a7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/schoollivewebsite/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/schoollivewebsite/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/schoollivewebsite/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/schoollivewebsite/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "56d141bb3d090798bdb4c9b0e773335a",
+    "url": "/schoollivewebsite/static/media/office.56d141bb.png"
+  },
+  {
+    "revision": "3dab63ca5ad596f948ddc6ce6318b1b5",
+    "url": "/schoollivewebsite/static/media/primary-school.3dab63ca.jpg"
+  },
+  {
+    "revision": "11d898848ca0f9047e42ca2362855de0",
+    "url": "/schoollivewebsite/static/media/principle.11d89884.png"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/schoollivewebsite/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "0eee6ee6edd9c6c6480b16970dfce2a0",
+    "url": "/schoollivewebsite/static/media/read.0eee6ee6.png"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/schoollivewebsite/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "fae9d23535e119c110b9bfc9a7728b2b",
+    "url": "/schoollivewebsite/static/media/scl-activities.fae9d235.jpg"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/schoollivewebsite/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "1d5951c80417588787f235eb4d86f303",
+    "url": "/schoollivewebsite/static/media/sports1.1d5951c8.jpg"
+  },
+  {
+    "revision": "be844e946a2ff2687fbbb1d82e0e3fb0",
+    "url": "/schoollivewebsite/static/media/sports10.be844e94.jpg"
+  },
+  {
+    "revision": "8c0772304ab6a48e75b57ee3d25f3e3c",
+    "url": "/schoollivewebsite/static/media/sports2.8c077230.jpg"
+  },
+  {
+    "revision": "e7f65daed2bced30581a837c53ade3e7",
+    "url": "/schoollivewebsite/static/media/sports3.e7f65dae.jpg"
+  },
+  {
+    "revision": "d3a8ae961aaab54c57d961fa07a5131e",
+    "url": "/schoollivewebsite/static/media/sports4.d3a8ae96.jpg"
+  },
+  {
+    "revision": "a632e2e749bd3b43d6152eb12c623996",
+    "url": "/schoollivewebsite/static/media/sports5.a632e2e7.jpg"
+  },
+  {
+    "revision": "6f7e5652355f80148507159587bb91ec",
+    "url": "/schoollivewebsite/static/media/sports6.6f7e5652.jpg"
+  },
+  {
+    "revision": "82d4eb5ddcecadad0f3e27b3950a8dc2",
+    "url": "/schoollivewebsite/static/media/sports7.82d4eb5d.jpg"
+  },
+  {
+    "revision": "a7a25187f721c9bb46d7e3996b104540",
+    "url": "/schoollivewebsite/static/media/sports8.a7a25187.jpg"
+  },
+  {
+    "revision": "671bc99e55b8d16bab37a393161c917d",
+    "url": "/schoollivewebsite/static/media/staff1.671bc99e.png"
+  },
+  {
+    "revision": "21016327d897084f41755bbbb146ce9e",
+    "url": "/schoollivewebsite/static/media/teacher2.21016327.png"
+  },
+  {
+    "revision": "f66a941d463da8a620f770cf751e39d9",
+    "url": "/schoollivewebsite/static/media/teacher3.f66a941d.png"
+  },
+  {
+    "revision": "fe34e7e936cedf47e5b94806e36a742e",
+    "url": "/schoollivewebsite/static/media/teacher4.fe34e7e9.png"
+  },
+  {
+    "revision": "3274cdaf34a226cffada8c07098483a9",
+    "url": "/schoollivewebsite/static/media/thinking.3274cdaf.png"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/schoollivewebsite/static/media/uriel-soberanes.fe3a7838.jpg"
+  },
+  {
+    "revision": "b4e240d1ab135a878a5f2d4d39d7df7c",
+    "url": "/schoollivewebsite/static/media/vision.b4e240d1.png"
+  },
+  {
+    "revision": "5c43c70c350c1fc6d52cfa07b0beaa1a",
+    "url": "/schoollivewebsite/static/media/vortex-5142615_1280.5c43c70c.png"
+  }
+]);
